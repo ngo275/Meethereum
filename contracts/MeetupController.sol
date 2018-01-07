39b50fc21d43d91c67a94be829877718f7b058f1
ngo275 <ngo38114@gmail.com>
2018-01-08 00:32:25 +0900

    added new methods to MeetupController

--- contracts/MeetupController.sol
+++ contracts/MeetupController.sol
@@ -7,6 +7,14 @@ contract MeetupController {
 
   address[] meetups;
   mapping (address => uint[]) organizerMeetups;
+  MeetupObject[] meetupObjects;
+
+  struct MeetupObject {
+    address identifier;
+    uint applicationStartedAt;
+    uint applicationEndedAt;
+    uint date;
+  }
     
   event MeetupSet(
     address meetupAddress,
@@ -24,6 +32,7 @@ contract MeetupController {
     uint idx = meetups.length;
     meetups.push(meetupAddress);
     organizerMeetups[_organizer].push(idx);
+    meetupObjects.push(MeetupObject(meetupAddress, _applicationStartedAt, _applicationEndedAt, _date));
     MeetupSet(meetupAddress, _organizer, _name, _place, _applicationStartedAt, _applicationEndedAt, _date, _minFee, _capacity);
   }
   
@@ -36,6 +45,81 @@ contract MeetupController {
     _meetups = new address[](oms.length);
     for (uint i = 0; i < oms.length; i++) {
       _meetups[i] = meetups[oms[i]];
+    }
+  }
+
+  function getUpcomingMeetups()
+    public
+    constant
+    returns (address[] _meetups)
+  {
+    uint fixedLength = 6;
+    uint length = (meetupObjects.length > fixedLength) ? fixedLength : meetupObjects.length;
+    _meetups = new address[](length);
+    uint skipCount = 0;
+    for (uint i = 0; i < length; i++) {
+      if (i + skipCount > meetupObjects.length) {
+        return _meetups;
+      }
+      for (uint j = i + skipCount; j < meetupObjects.length; j++) {
+        if (meetupObjects[j].date > now) {
+          _meetups[i] = meetupObjects[j].identifier;
+          break; // 内側のforだけ抜けたい
+        } else {
+          skipCount += 1;
+        }
+      }
+    }
+  }
+
+  function getUpcomingMeetupObjects()
+    public
+    constant
+    returns (MeetupObject[] _meetups)
+  {
+    uint fixedLength = 6;
+    uint length = (meetupObjects.length > fixedLength) ? fixedLength : meetupObjects.length;
+    _meetups = new MeetupObject[](length);
+    uint skipCount = 0;
+    for (uint i = 0; i < length; i++) {
+      if (i + skipCount > meetupObjects.length) {
+        return sortedMeetups(_meetups);
+      }
+      for (uint j = i + skipCount; j < meetupObjects.length; j++) {
+        if (meetupObjects[j].date > now) {
+          _meetups[i] = meetupObjects[j];
+          break; // 内側のforだけ抜けたい
+        } else {
+          skipCount += 1;
+        }
+      }
+    }
+    return sortedMeetups(_meetups);
+  }
+
+  function sortedMeetups(MeetupObject[] _meetups)
+    private
+    constant
+    returns (MeetupObject[] sorted)
+  {
+    uint len = _meetups.length;
+    uint i = 0;
+    uint j = 0;
+
+    sorted = new MeetupObject[](len);
+
+    for(i = 0; i < len; i++) {
+      sorted[i] = _meetups[i];
+    }
+
+    for(i = 1; i < sorted.length; i++ ) {
+      var c = sorted[i];
+
+      for(j = i; j > 0 && sorted[j-1].date < c.date; j-- ) {
+        sorted[j] = sorted[j-1];
+      }
+
+      sorted[j] = c;
     }
   }
 }
